13057cbd676d7d3a06c2bfd976a5415f8d06a4d9
Michael Salim <salim_michaelmi@yahoo.com>
2022-05-03 23:31:23 +0100

    Feat: Display drafts on sidebar

--- web/src/containers/SharedLayout/index.tsx
+++ web/src/containers/SharedLayout/index.tsx
@@ -2,11 +2,17 @@ import { Box, Flex, Text } from "@chakra-ui/react";
 import React from "react";
 import { Outlet } from "react-router-dom";
 
+import { useAllTweetDrafts } from "api/tweetDrafts/useAllTweetDrafts";
+
 export default function SharedLayout() {
+  const { data } = useAllTweetDrafts();
+
   return (
     <Flex minH="100vh" alignItems="stretch">
-      <Box w="200px" borderRight="1px solid" borderColor="gray.700">
-        <Text>Sidebar</Text>
+      <Box w="300px" borderRight="1px solid" borderColor="gray.700">
+        {data?.map((d) => (
+          <Text>{d.data[0].text}</Text>
+        ))}
       </Box>
       <Box flex={1}>
         <Outlet />
